107935e5e62172f6ec180cb92fd1afcd4eeae49b
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2021-10-08 17:28:14 -0300

    exercise 2 completed

--- exercises/01_variables_and_opertors.js
+++ exercises/01_variables_and_opertors.js
@@ -1,15 +1,15 @@
-// a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.//
+// a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
 var jordan = 23;
 var ronaldo = 7;
 var messi = jordan + ronaldo
-console.log("the answer of exercise 1-a is " + messi)
-// b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.//
+console.log("1-a the answer is " + messi)
+// b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
 var myName = "andres "
 var lastName = "cespedes"
 var fullName = myName + lastName
-console.log("the answer of exercise 1-b is " + fullName)
-// c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).//
+console.log("1-b the answer is " + fullName)
+// c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
 var cristiano = "ronaldo"
 var lionel = "messi"
 var theBests = cristiano.length + lionel.length
-console.log("the answer of exercise 1-c is " + theBests)
+console.log("1-c the answer is " + theBests)
