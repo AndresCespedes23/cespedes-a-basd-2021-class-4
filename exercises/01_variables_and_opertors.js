5117775be6c253b72c49d5e947c4816b0288f0bb
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2021-10-09 10:40:46 -0300

    exercise 3 completed

--- exercises/01_variables_and_opertors.js
+++ exercises/01_variables_and_opertors.js
@@ -2,14 +2,14 @@
 var jordan = 23;
 var ronaldo = 7;
 var messi = jordan + ronaldo
-console.log("1-a the answer is " + messi)
+console.log("1-a the answer is: " + messi)
 // b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
 var myName = "andres "
 var lastName = "cespedes"
 var fullName = myName + lastName
-console.log("1-b the answer is " + fullName)
+console.log("1-b the answer is: " + fullName)
 // c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
 var cristiano = "ronaldo"
 var lionel = "messi"
 var theBests = cristiano.length + lionel.length
-console.log("1-c the answer is " + theBests)
+console.log("1-c the answer is: " + theBests)
