ed25ff7194a877b05ea9c5cc9595a3004fa10917
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2021-10-11 22:53:04 -0300

    exercise 5 completed

--- exercises/05_for.js
+++ exercises/05_for.js
@@ -8,6 +8,14 @@ for (i = 0; i < fridge.length; i++) {
     console.log('5-b the answer is ' + fridge[i].substring(0, 1).toUpperCase() + fridge[i].substring(1))
 }
 //c) Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a) recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence. Al final mostrar una única alerta con la cadena completa.
-
+var sentence = [];
+for (i = 0; i < fridge.length; i++) {
+    sentence = sentence + fridge[i]
+}
+console.log('5-c the answer is ' + sentence)
 //d) Crear una array vacío y con un bucle for de 10 repeticiones. Llenar el array con el número de la repetición, es decir que al final de la ejecución del bucle for debería haber 10 elementos dentro del array, desde el número 0 hasta al número 9. Mostrar por la consola del navegador el array final (utilizar console.log).
-
+var emptyArray = [];
+for (i = 0; i < 10; i++) {
+    emptyArray = emptyArray + [i];
+}
+console.log('5-d the answer is: ' + emptyArray)
